b2a954e62e54941468a92c27fad23588ddbf0f3a
ctro <clint@ctro.net>
2019-10-11 14:54:29 -0600

    switch up ci

---
+++ .github/actions/jekyll/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Any error will fail the script.
+set -e
+
+# Entrypoint for Github Acitons when it runs our Docker containers.
+#  this is Github convention
+
+cd ${GITHUB_WORKSPACE}
+echo 🦷 pwd: $(pwd)
+echo ‍‍🦷 ls workspace: $(ls)
+
+# Install the bundle
+bundle install
+
+# Build the jekyll site
+bundle exec jekyll build
+
+# Test the generated site
+htmlproofer docs/_site
+
+echo 🦷 ls site: $(ls _site)
--- Gemfile
+++ Gemfile
@@ -2,4 +2,6 @@ source "https://rubygems.org"
 
 gem "jekyll"
 gem "github-pages", group: :jekyll_plugins
+gem "jekyll-seo-tag"
+
 gem "html-proofer"
